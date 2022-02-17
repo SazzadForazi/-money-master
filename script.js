68e43cccbaa4e6fa100926872e653bfeb716080a
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-02-17 18:20:11 +0600

    first commit

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 const calculateValue = document.getElementById("calculate").addEventListener('click', function () {
-    const food = document.getElementById('food');
-    const foodPrice = food.value;
-    const rent = document.getElementById('rent');
-    const rentPrice = rent.value;
-    const clothes = document.getElementById('clothes');
-    const clothesPrice = clothes.value;
+    const foodPrice = document.getElementById('food').value;
+    const rentPrice = document.getElementById('rent').value;
+    const clothesPrice = document.getElementById('clothes').value;
     const totalexprenses = parseFloat(foodPrice) + parseFloat(rentPrice) + parseFloat(clothesPrice);
     document.getElementById('exprenses').innerText = totalexprenses;
-    // document.getElementById(remaining).innerText = 
-    // console.log(totalexprenses);
-    // console.log(foodPrice, rentPrice, clothesPrice);
+
+    if (foodPrice.value == "" ||
+        foodPrice.value < 0 ||
+        rentPrice.value == "" ||
+        rentPrice.value < 0 ||
+        clothesPrice.value == "" ||
+        clothesPrice.value < 0
+    ) {
+        console.log("please trying to vaild Input");
+        return;
+    }
+
+
+
+    const income = document.getElementById('income');
+    const totalIncome = parseFloat(income.value);
+
+    const remaining = totalIncome - totalexprenses;
+    document.getElementById('remaining').innerText = remaining;
+
+    document.getElementById('save-btn').addEventListener('click', function () {
+        const save = document.getElementById('save').value;
+        const discount = parseFloat(totalIncome) * save / 100;
+        const totaldiscount = document.getElementById('total-saving').innerText = discount;
+        // console.log(totaldiscount)
+        const remainingBalance = parseFloat(remaining) - parseFloat(totaldiscount);
+        document.getElementById('total-remaining').innerText = remainingBalance;
+    })
+
 })
