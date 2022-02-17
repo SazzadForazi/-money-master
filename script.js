8bedd08a5992064cd06ca80c804cfc522bb2990a
Sazzad Hossain <sazzad.csebubt@gmail.com>
2022-02-17 23:34:24 +0600

    first commit

--- script.js
+++ script.js
@@ -1,36 +1,44 @@
-const calculateValue = document.getElementById("calculate").addEventListener('click', function () {
+const calculateValue = document.getElementById("calculate").addEventListener('click', function (e) {
+    e.preventDefault()
+
+
     const foodPrice = document.getElementById('food').value;
+
     const rentPrice = document.getElementById('rent').value;
+
     const clothesPrice = document.getElementById('clothes').value;
-    const totalexprenses = parseFloat(foodPrice) + parseFloat(rentPrice) + parseFloat(clothesPrice);
+
+    let totalexprenses = parseFloat(foodPrice) + parseFloat(rentPrice) + parseFloat(clothesPrice);
     document.getElementById('exprenses').innerText = totalexprenses;
-
-    if (foodPrice.value == "" ||
-        foodPrice.value < 0 ||
-        rentPrice.value == "" ||
-        rentPrice.value < 0 ||
-        clothesPrice.value == "" ||
-        clothesPrice.value < 0
-    ) {
-        console.log("please trying to vaild Input");
-        return;
-    }
-
-
-
     const income = document.getElementById('income');
     const totalIncome = parseFloat(income.value);
 
     const remaining = totalIncome - totalexprenses;
     document.getElementById('remaining').innerText = remaining;
 
+
+    if ((isNaN(parseFloat(foodPrice)) || parseFloat(foodPrice) < 0) ||
+        (isNaN(parseFloat(rentPrice)) || parseFloat(rentPrice) < 0) ||
+        (isNaN(parseFloat(clothesPrice)) || parseFloat(clothesPrice) < 0)) {
+        return alert('Please try to valid input');
+    }
+    else if (totalIncome < totalexprenses) {
+        return alert("please try to valid input");
+    }
+
+
     document.getElementById('save-btn').addEventListener('click', function () {
-        const save = document.getElementById('save').value;
+
+        let save = document.getElementById('save').value;
         const discount = parseFloat(totalIncome) * save / 100;
         const totaldiscount = document.getElementById('total-saving').innerText = discount;
         // console.log(totaldiscount)
         const remainingBalance = parseFloat(remaining) - parseFloat(totaldiscount);
         document.getElementById('total-remaining').innerText = remainingBalance;
+        if (save < remaining) {
+            return alert("Not Enough money");
+        }
+
     })
 
 })
